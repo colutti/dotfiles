b003f625ac12d4c30721ede9528f139f53db44d4
Rafael Colucci <rafacolucci@gmail.com>
2025-06-25 18:39:19 +0200

    changes

---
+++ scripts/steam_gamescope.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Verifica se o ambiente não é Hyprland
+if [ -z "$HYPRLAND_INSTANCE_SIGNATURE" ]; then
+    # Executa o gamescope com os parâmetros especificados
+    exec gamescope -W 3840 -H 2160 -f --hdr-enabled --mangoapp -- "$@"
+else
+    # Executa o comando original se estiver no Hyprland
+    exec "$@"
+fi
