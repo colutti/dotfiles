c3c512fce3972b9068959ad13b69e911c29d9a20
Rafael Colucci <rafacolucci@gmail.com>
2025-11-14 07:27:21 +0100

    changes

--- install.sh
+++ install.sh
@@ -2,29 +2,74 @@
 
 # Lista de pacotes para instalar (pode editar facilmente)
 PACKAGES=(
-  stow
-  xdg-desktop-portal-hyprland
-  xdg-desktop-portal-wlr
-  jq
-  dms-git
-  kitty
-  steam
-  telegram-desktop
+  ags-hyprpanel-git
+  aylurs-gtk-shell-git
+  bluez
+  bluez-utils
+  brightnessctl
+  btop
+  cargo
+  codium
+  dart-sass
   discord
   firefox
   gamemode
+  grimblast-git
+  gvfs
+  gtksourceview3
+  hyprland
+  hyprpicker
+  hyprsunset-git
+  jq
+  kitty
+  lazydocker
+  lazygit
+  libgtop
+  libsoup3
   mangohud
-  codium
+  matugen-bin
+  networkmanager
+  nodejs
+  nvim
+  pacman-contrib
+  power-profiles-daemon
+  python
+  python-gpustat
+  pywal-git
+  steam
+  stow
+  swww
+  telegram-desktop
+  terminus-font
+  upower
+  wf-recorder-git
+  wireplumber
+  wl-clipboard
+  xdg-desktop-portal
+  xdg-desktop-portal-gtk
+  xdg-desktop-portal-hyprland
+  xdg-desktop-portal-wlr
+  lm_sensors
+  yazi
+  hypridle
+  hyprlock
+  qt5-wayland
+  qt6-wayland
+  ttf-liberation
+  noto-fontr
+  noto-fonts-emoji
+  ttf-carlito
+  ttf-caladea
+  ttf-ms-fonts
+  walker
+  elephant
+  elephant-desktopapplications
+  github-cli
+  ly
 )
-
 # Instala todos os pacotes necessários
 paru -S --needed --noconfirm "${PACKAGES[@]}"
 
-# Aplica todos os dotfiles usando Stow, removendo antes para garantir sobrescrita
-cd ~/dotfiles
-for pkg in *; do
-  stow -D $pkg   # Remove symlinks/arquivos existentes
-  stow $pkg      # Cria novos symlinks
-done
-
 echo "Instalação e configuração dos dotfiles concluída!"
+
+systemctl enable ly.service
